94eaf06d0bac1ac2d4770ba94082c80c593ac2b6
jerryye <845460348@qq.com>
2017-12-17 15:39:19 +0800

    提交博客文章

--- commit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "提交博客文章"
--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git add .
+git commit -m "提交博客文章"
+
 echo 'Push to github master'
 git push github master
 echo 'Push to coding master'
