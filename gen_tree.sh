c1e8170ff02e93abaddc8fe15fcb04ced803cf8f
jerryye <845460348@qq.com>
2018-03-19 17:29:33 +0800

    提交博客文章

--- gen_tree.sh
+++ gen_tree.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #生成目录树,只有目录
-#tree  -d  -N -I "image*|file*"  -o temp.txt
+tree  -d  -N -I "image*|file*"  -o temp.txt
 #生成目录树, 有目录和文件名
-tree   -N -I "image*|file*"  -o temp.txt
+#tree   -N -I "image*|file*"  -o temp.txt
